c65888d0f5ba0c912e26d43d8d93980ea6282267
michael1011 <me@michael1011.at>
2019-01-20 16:52:09 +0100

    feat: use environment variables for API and networks

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 import { Networks } from 'boltz-core';
 
-export const boltzApi = 'http://localhost:9001';
+export const boltzApi = process.env.REACT_APP_BOLTZ_API;
 
-export const bitcoinNetwork = Networks.bitcoinSimnet;
-export const litecoinNetwork = Networks.litecoinSimnet;
+export const bitcoinNetwork = Networks[process.env.REACT_APP_BITCOIN_NETWORK];
+export const litecoinNetwork = Networks[process.env.REACT_APP_LITECOIN_NETWORK];
