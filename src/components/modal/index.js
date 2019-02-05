a7230e790f37d36f55a6348a78c5d2d53ba5a233
michael1011 <me@michael1011.at>
2019-02-05 17:57:14 +0100

    fix: minor wording changes

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -4,25 +4,26 @@ import PropTypes from 'prop-types';
 
 const ModalComponent = props => {
   const { isOpen, onClose } = props;
+
   return (
     <Modal open={isOpen} onClose={onClose} center>
       <div style={{ fontSize: '20px' }}>
-        <p>On 4th of September 2018,</p>
         <p>
-          In a{' '}
+          On 4th of September 2018, in a{' '}
           <a href="https://info.shapeshift.io/blog/2018/09/04/introducing-shapeshift-membership/">
             blogpost
           </a>
           , Shapeshift, one of the largest cryptocurrency entities, scummed to
           user data collection.
-          <br />
-          By creating an account on a custodial exchange like Shapeshift, you
-          are giving government and anyone who can access that KYC data, the
-          power to not only know that you have crypto assets but confiscate them
-          during a trade.
         </p>
         <p>
-          We built Boltz with a dream of a fairer financial world, with a
+          By creating an account on a custodial exchange like Shapeshift, you
+          are giving the government and anyone who can access that KYC data, the
+          power to not only know that you have crypto assets but also to
+          confiscate them during a trade.
+        </p>
+        <p>
+          We built Boltz with a dream of a fairer financial world, with the
           primary goal to empower users with financial sovereignty. Therefore,{' '}
           <b>
             Boltz does not and will never collect any personal data from users
@@ -30,14 +31,13 @@ const ModalComponent = props => {
           .
         </p>
         <p>
-          Also, Boltz leverages advanced cryptography (atomic swaps) in a way,
-          so that trades either complete in full or get refunded automatically.
-          Users can rest assured to be in possession of their funds at all
-          times, without worrying about who is behind Boltz and if this entity
-          is trustworthy.
+          Also, Boltz leverages atomic swaps in a way, so that trades either
+          complete in full or get refunded. Users can rest assured to be in
+          possession of their funds at all times, without worrying about who is
+          behind Boltz and if this entity is trustworthy.
         </p>
         <p>
-          Because trading <b>{`shouldn't`}</b> require an account.
+          Trading <b>{`shouldn't`}</b> require an account.
         </p>
       </div>
     </Modal>
