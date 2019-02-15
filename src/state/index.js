c151ddb085082a8699ce3ce5b469d4b172ee4d43
ImmanuelSegol <3ditds@gmail.com>
2019-02-15 19:07:09 +0200

    feat: dont include middelware in production build

--- src/state/index.js
+++ src/state/index.js
@@ -1,12 +1,17 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
 import createRootReducer from './rootReducer';
-
 import logger from 'redux-logger';
+
+let middelware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middelware = [...middelware, logger];
+}
 
 const store = createStore(
   createRootReducer(),
-  compose(applyMiddleware(logger, thunk))
+  compose(applyMiddleware(...middelware))
 );
 
 export default store;
