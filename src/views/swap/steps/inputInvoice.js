4820705b73a81eee7c961134db0147d98f2bc6ca
michael1011 <me@michael1011.at>
2019-03-11 19:08:34 +0100

    refactor: show amount of requested invoice in sats

--- src/views/swap/steps/inputInvoice.js
+++ src/views/swap/steps/inputInvoice.js
@@ -5,9 +5,10 @@ import { FaBolt } from 'react-icons/fa';
 import View from '../../../components/view';
 import InputArea from '../../../components/inputarea';
 import {
+  toSatoshi,
   getCurrencyName,
   getSampleInvoice,
-  toSatoshi,
+  getSmallestDenomination,
 } from '../../../scripts/utils';
 
 const InputInvoiceStyles = () => ({
@@ -75,7 +76,8 @@ class StyledInputInvoice extends React.Component {
           Paste a <b>{getCurrencyName(swapInfo.quote)}</b> Lightning {}
           <FaBolt size={30} color="#FFFF00" /> invoice for <br />
           <b>
-            {swapInfo.quoteAmount} {swapInfo.quote}
+            {toSatoshi(swapInfo.quoteAmount)}{' '}
+            {getSmallestDenomination(swapInfo.quote)}
           </b>
         </p>
         <InputArea
